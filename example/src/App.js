2f28af617da3f30d0bd21d65c62cab4bcdbf6e84
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-11-16 10:21:18 +1100

    Added more breadcrumb examples

--- example/src/App.js
+++ example/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 
 import Breadcrumbs from 'voltage-breadcrumbs'
 
@@ -8,12 +8,38 @@ const links = [
   { link: "http://google.com", text: "Security details" },
 ]
 
+const moreLinks = [
+  { link: "http://google.com", text: "Home" },
+  { link: "http://google.com", text: "Account information" },
+  { link: "http://google.com", text: "Security details" },
+  { link: "http://youtube.com", text: "Change Account Password" },
+  { link: "http://youtube.com", text: "Confirm change" }
+]
+
+const Link = ({ text }) => (
+  <a href="#">{text}</a>
+)
+
+const customLinks = [
+  <Link text="Account information" />,
+  <Link text="Security details" />,
+  <Link text="Confirm change" />
+]
+
 export default class App extends Component {
   render () {
     return (
-      <div>
-        <Breadcrumbs links={links} />
-      </div>
+      <Fragment>
+        <div>
+          <Breadcrumbs links={links} />
+        </div>
+        <div style={{ marginTop: "20px" }}>
+          <Breadcrumbs links={moreLinks} />
+        </div>
+        <div style={{ marginTop: "20px" }}>
+          <Breadcrumbs links={customLinks} />
+        </div>
+      </Fragment>
     )
   }
 }
