81703746e84dd3fc09a0d2746c9a0bb1048e5e09
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-11-15 22:08:42 +1100

    Basic breadcrumb component

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,66 @@
  * @class ExampleComponent
  */
 
-import React, { ReactElement } from 'react'
+import React, { ReactElement, AnchorHTMLAttributes } from 'react'
 
 import styles from './styles.css'
 
-export type BreadcrumbProps = {
+export interface BreadcrumbLink extends AnchorHTMLAttributes<HTMLAnchorElement> {
+  link: string
   text: string
 }
 
-const Breadcrumbs = ({ text }: BreadcrumbProps): ReactElement => {
-  return (
-    <nav className={styles.voltage__breadcrumbs}>
+type LinkOrReactElement = BreadcrumbLink
 
+export type BreadcrumbProps = {
+  links: Array<LinkOrReactElement>
+  separator: string|null
+}
+
+const BreadcrumbLink = ({ link, text, ...props }: BreadcrumbLink): ReactElement => (
+  <a href={link} {...props}>{text}</a>
+)
+
+const Breadcrumbs = ({ links, separator = "/" }: BreadcrumbProps): ReactElement => {
+
+  const isLastIterator = (i: number): boolean => {
+    return i === links.length - 1;
+  }
+
+  const breadcrumbLinkClassName = (i: number): string => {
+    const classes = []
+    isLastIterator(i) && classes.push(styles["voltage__breadcrumbs__link--active"])
+    !isLastIterator(i) && classes.push(styles["voltage__breadcrumbs__link--truncate"])
+
+    return classes.join(" ").trim()
+  }
+
+  return (
+    <nav className={`voltage__breadcrumbs`}>
+      {
+        links && (
+          <ul className={styles.voltage__breadcrumbs}>
+            {
+              links.map((link, i): ReactElement => (
+                link ? (
+                  <li key={i}>
+                    <BreadcrumbLink
+                      link={link.link}
+                      text={link.text}
+                      className={breadcrumbLinkClassName(i)}
+                    />
+                    {
+                      !isLastIterator(i) && separator ? <span>{separator}</span> : ""
+                    }
+                  </li>
+                ) : (
+                  <></>
+                )
+              ))
+            }
+          </ul>
+        )
+      }
     </nav>
   )
 }
